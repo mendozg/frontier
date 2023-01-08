c0fb86e007499c86a231166e568908b0029a5d4d
mendozg <35119606+mendozg@users.noreply.github.com>
2023-01-07 21:58:17 -0600

    Update schema_v3_override.rs

--- client/rpc/src/overrides/schema_v3_override.rs
+++ client/rpc/src/overrides/schema_v3_override.rs
@@ -58,9 +58,12 @@ where
 	BE::State: StateBackend<BlakeTwo256>,
 {
 	fn query_storage<T: Decode>(&self, id: &BlockId<B>, key: &StorageKey) -> Option<T> {
-		if let Ok(Some(data)) = self.client.storage(header.hash(), key) {
-			if let Ok(result) = Decode::decode(&mut &data.0[..]) {
-				return Some(result);
+		let hash = client.hash(id);
+		if let Ok(Some(header)) = self.client.header(hash) {
+			if let Ok(Some(data)) = self.client.storage(header.hash(), key) {
+				if let Ok(result) = Decode::decode(&mut &data.0[..]) {
+					return Some(result);
+				}
 			}
 		}
 		None
